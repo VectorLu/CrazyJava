adf7bf38d21141474eb23ef6643423e22c5e493c
VectorLu <vectorpinglu@outlook.com>
2016-06-20 12:57:10 +0800

    the 0th day hhh

---
+++ CH1/HelloUTF.java
@@ -0,0 +1,7 @@
+public class HelloUTF
+{
+    public static void main(String[] args) {
+        System.out.println("HelloUTF");
+        //测试UTF-8
+    }
+}
---
+++ CH1/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        /**
+        *测试javadoc
+        */
+        System.out.println("Hello javadoc");
+        System.out.println("HelloJava!");
+    }
+}
